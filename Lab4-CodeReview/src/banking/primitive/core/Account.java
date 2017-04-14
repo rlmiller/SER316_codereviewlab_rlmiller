adda13f044b2844ceedb578f114a1b80f3a29e3d
rlmiller <rlmiller@asu.edu>
2017-04-13 20:05:41 -0500

    bug-1

--- Lab4-CodeReview/src/banking/primitive/core/Account.java
+++ Lab4-CodeReview/src/banking/primitive/core/Account.java
@@ -1,3 +1,14 @@
+/*
+  File:	Account.Java
+  Author: Some coder person
+  Date:	a date before 04.13.17
+  
+  Description: This is the account Class that is inherited by Checking.java and Savings.java
+*/
+
+
+
+
 package banking.primitive.core;
 
 public abstract class Account implements java.io.Serializable {
